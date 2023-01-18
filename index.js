4914fa5b8bc9a131f9458e88e07b1fd4d501a5ed
Boby900 <bobytiwari310@gmail.com>
2023-01-18 15:11:00 +0530

    added the project to the github

---
+++ index.js
@@ -0,0 +1,19 @@
+const firstNum = document.getElementById("first-num")
+const secondNum = document.getElementById("second-num")
+const addition = document.querySelector(".first")
+const substraction = document.querySelector(".second")
+const multiply = document.querySelector(".third")
+const divide = document.querySelector(".fourth")
+
+let = function plus(value) {
+    return num1 + num2
+}
+let = function minus(value){
+    return num1 - num2
+}
+let = function mul(value){
+    return num1 * num2
+}
+let = function div(value){
+    return num1 / num2
+}
